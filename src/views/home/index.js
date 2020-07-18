9719702abb0d861fa84befb6c87bd231878d136a
TheVictoor <victoorsp@gmail.com>
2020-07-18 16:17:10 -0300

    Mobile version Fylo Storage

---
+++ src/views/home/index.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import './home.css';
+import logo from './../../assets/logo.svg';
+import iconDocument from './../../assets/icon-document.svg'
+import iconFolder from './../../assets/icon-folder.svg'
+import iconUpload from './../../assets/icon-upload.svg'
+
+function Features(){
+  return (
+    <div className="features">
+      <div className="brand">
+        <img src={logo} alt="logo da empresa"/>
+      </div>
+      <div className="icons">
+        <div className="icon">
+          <img src={iconDocument} alt="Icone de documento"/>
+        </div>
+        <div className="icon">
+          <img src={iconFolder} alt="icone de pasta"/>
+        </div>
+        <div className="icon">
+          <img src={iconUpload} alt="icone de upload"/>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function Storage () {
+  return (
+    <>
+      <div className="storage">
+        <span className="usage-info">
+          You’ve used  <span className='text-bold'> 815 GB </span> of your storage
+        </span>
+        <div className="memory-used">
+          <div className="memory-used-bar">
+            {/* style={{ width: `${81}%` }} */}
+            <div className="current-usage" style={{ width: `${81.5}%` }}>
+              <div className="circle"></div>
+            </div>
+          </div>
+          <div className="limit-legend">
+            <span className='text-bold'> 0 GB </span>
+            <span className='text-bold'> 1000 GB </span>
+          </div>
+        </div>
+      </div>
+      <div className="memory-left">
+        <span className='big-bold'> 185 </span>
+        <span className='light-gray'> GB LEFT </span>
+      </div>
+    </>
+  );
+}
+
+function Home() {
+  return (
+    <div className="home">
+      <Features />
+      <Storage />
+    </div>
+  );
+}
+
+export default Home;
